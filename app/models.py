a6064b6af8e92ade7a2081a8f25fdd5020bee5f2
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-02-11 19:03:29 +0530

    models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. 
+'''class Profile(models.Model):
+    name = models.CharField(max_length=30)    
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=50)
+    address = models.CharField(max_length=200)
+    address2 = models.CharField(max_length=200)
+    city = models.CharField(max_length=15)
+    state = models.CharField(max_length=20)
+    zip_code = models.CharField(max_length=6)'''
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=50)
+    #product_price = models.IntegerField()
+    product_category = models.CharField(max_length=50)
+    product_description = models.CharField(max_length=1000)
+
+
+
+    
