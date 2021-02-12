6deb969c0f2fa1a7c0220340a32fd3a020b758a8
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-02-12 17:39:20 +0530

    db created

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
-
+from .models import (
+    Customer, 
+    Product,
+    Cart,
+    OrderPlaced
+)
 # Register your models here.
+
+@admin.register(Customer)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display=['id' , 'user' , 'name' , 'locality' , 'city' , 'state' , 'zipcode']
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display=['id' ,'title' , 'description' , 'selling_price' , 'discounted_price' , 'category' , 'brand', 'product_image']
+
+@admin.register(Cart)
+class CartModelAdmin(admin.ModelAdmin):
+    list_display=['id' , 'product' , 'quantity']
+
+@admin.register(OrderPlaced)
+class OrderPlacedModelAdmin(admin.ModelAdmin):
+    list_display=['id' , 'user' , 'customer' , 'product' , 'quantity' , 'ordered_date' , 'status']
+
--- app/models.py
+++ app/models.py
@@ -5,9 +5,7 @@ from django.core.validators import MaxValueValidator,MinValueValidator
 # Create your models here. 
 
 STATE_CHOICES =(
-    ('Andaman $ Nicobar' , 'Andaman $ Nicobar'),
-    ('Andhra Pradesh' , 'Andhra Pradesh'),
-    ('Arunachal Pradesh' , 'Arunachal Pradesh')
+    ("Andhra Pradesh","Andhra Pradesh"),("Arunachal Pradesh ","Arunachal Pradesh "),("Assam","Assam"),("Bihar","Bihar"),("Chhattisgarh","Chhattisgarh"),("Goa","Goa"),("Gujarat","Gujarat"),("Haryana","Haryana"),("Himachal Pradesh","Himachal Pradesh"),("Jammu and Kashmir ","Jammu and Kashmir "),("Jharkhand","Jharkhand"),("Karnataka","Karnataka"),("Kerala","Kerala"),("Madhya Pradesh","Madhya Pradesh"),("Maharashtra","Maharashtra"),("Manipur","Manipur"),("Meghalaya","Meghalaya"),("Mizoram","Mizoram"),("Nagaland","Nagaland"),("Odisha","Odisha"),("Punjab","Punjab"),("Rajasthan","Rajasthan"),("Sikkim","Sikkim"),("Tamil Nadu","Tamil Nadu"),("Telangana","Telangana"),("Tripura","Tripura"),("Uttar Pradesh","Uttar Pradesh"),("Uttarakhand","Uttarakhand"),("West Bengal","West Bengal"),("Andaman and Nicobar Islands","Andaman and Nicobar Islands"),("Chandigarh","Chandigarh"),("Dadra and Nagar Haveli","Dadra and Nagar Haveli"),("Daman and Diu","Daman and Diu"),("Lakshadweep","Lakshadweep"),("National Capital Territory of Delhi","National Capital Territory of Delhi"),("Puducherry","Puducherry")
 )
 class Customer(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
@@ -28,13 +26,13 @@ CATEGORY_CHOICES =(
 )
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=50)
-    product_price = models.FloatField()
-    discount_price = models.FloatField()
-    product_category = models.CharField(choices = CATEGORY_CHOICES , max_length=2)
-    product_description = models.TextField(max_length=1000)
-    product_brand = models.CharField(max_length=100)
-    product_image = models.ImageField(upload_to='productimg')
+    title = models.CharField(max_length=50)
+    selling_price = models.FloatField(default=None)
+    discounted_price = models.FloatField(default=None)
+    category = models.CharField(choices = CATEGORY_CHOICES , max_length=2 , null=True)
+    description = models.TextField(null=True)
+    brand = models.CharField(max_length=100 , null = True)
+    product_image = models.ImageField(upload_to='productimg' , null = True)
 
     def __str__(self):
         return str(self.id)
@@ -61,9 +59,9 @@ class OrderPlaced(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
-    ordered_date = models.DataTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=50,
-    choices=STATE_CHOICES,default='Pending')
+    choices=STATUS_CHOICES,default='Pending')
 
 
     
