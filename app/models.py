5b3da5ad6d9dc7bc0f7f62a58e8db0acc4e8ffb9
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-02-12 16:01:24 +0530

    models change

--- app/models.py
+++ app/models.py
@@ -1,21 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator,MinValueValidator
 
 # Create your models here. 
-'''class Profile(models.Model):
-    name = models.CharField(max_length=30)    
-    email = models.EmailField(max_length=50)
-    password = models.CharField(max_length=50)
-    address = models.CharField(max_length=200)
-    address2 = models.CharField(max_length=200)
+
+STATE_CHOICES =(
+    ('Andaman $ Nicobar' , 'Andaman $ Nicobar'),
+    ('Andhra Pradesh' , 'Andhra Pradesh'),
+    ('Arunachal Pradesh' , 'Arunachal Pradesh')
+)
+class Customer(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)   
+    locality = models.CharField(max_length=500)
     city = models.CharField(max_length=15)
-    state = models.CharField(max_length=20)
-    zip_code = models.CharField(max_length=6)'''
+    state = models.CharField(choices = STATE_CHOICES, max_length=50)
+    zipcode = models.IntegerField()
+
+    def __str__(self):
+        return str(self.id)
+
+CATEGORY_CHOICES =(
+    ('M', 'Mobile'),
+    ('L','Laptop'),
+    ('TW','Top Wear'),
+    ('BW', 'Bottom Wear'),
+)
 
 class Product(models.Model):
     product_name = models.CharField(max_length=50)
-    #product_price = models.IntegerField()
-    product_category = models.CharField(max_length=50)
-    product_description = models.CharField(max_length=1000)
+    product_price = models.FloatField()
+    discount_price = models.FloatField()
+    product_category = models.CharField(choices = CATEGORY_CHOICES , max_length=2)
+    product_description = models.TextField(max_length=1000)
+    product_brand = models.CharField(max_length=100)
 
 
 
