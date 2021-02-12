c7eb2694a90809a8a482d7861e0fde4e16ee795a
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-02-12 16:26:18 +0530

    models modify

--- app/models.py
+++ app/models.py
@@ -34,7 +34,36 @@ class Product(models.Model):
     product_category = models.CharField(choices = CATEGORY_CHOICES , max_length=2)
     product_description = models.TextField(max_length=1000)
     product_brand = models.CharField(max_length=100)
+    product_image = models.ImageField(upload_to='productimg')
 
+    def __str__(self):
+        return str(self.id)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+STATUS_CHOICES = (
+    ('Accepted', 'Accepted'),
+    ('Packed', 'Packed'),
+    ('Pending', 'Pending'),
+    ('On The Way', 'On The Way'),
+    ('Delivered','Delivered'),
+    ('Cancel', 'Cancel')
+)
+
+class OrderPlaced(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    ordered_date = models.DataTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50,
+    choices=STATE_CHOICES,default='Pending')
 
 
     
